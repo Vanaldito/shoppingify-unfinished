6219710c7d5f48c0dcc635962b8c095435a1e5a7
Vanaldito <fredimanuelb@gmail.com>
2023-01-03 19:01:49 -0600

    feat: add an api endpoint to check the auth status

--- server/src/routes/api.route.ts
+++ server/src/routes/api.route.ts
@@ -1,8 +1,31 @@
 import { Router } from "express";
+import { verifyAuthToken } from "../helpers";
 import usersRouter from "./users.route";
 
 const apiRouter = Router();
 
 apiRouter.use("/users", usersRouter);
+
+apiRouter.get("/auth-status", (req, res) => {
+  const authTokenCookie = req.cookies["auth-token"];
+
+  if (!authTokenCookie || typeof authTokenCookie !== "string") {
+    return res.json({ status: 200, data: { authenticated: false } });
+  }
+
+  const authToken = authTokenCookie.split(" ")[1];
+
+  if (!authToken) {
+    return res.json({ status: 200, data: { authenticated: false } });
+  }
+
+  try {
+    verifyAuthToken(authToken);
+
+    res.json({ status: 200, data: { authenticated: true } });
+  } catch {
+    res.json({ status: 200, data: { authenticated: false } });
+  }
+});
 
 export default apiRouter;
