f9c8e24cafe6249cb009b576e693c658ba3d36f3
Vanaldito <fredimanuelb@gmail.com>
2023-01-05 11:24:48 -0600

    feat: allow to set a label in the FieldForm component

--- client/src/components/FormField/FormField.tsx
+++ client/src/components/FormField/FormField.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import "./FormField.css";
 
+interface FormFieldProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label?: string;
+}
+
 export default function FormField({
+  label,
   className,
   ...props
-}: React.DetailedHTMLProps<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
->) {
-  className = `form-field ${className ?? ""}`.trim();
+}: FormFieldProps) {
+  const [isFocused, setIsFocused] = useState(false);
 
-  return <input className={className} {...props} />;
+  className = `form-field ${isFocused ? "form-field--focused" : ""} ${
+    className ?? ""
+  }`.trim();
+
+  return (
+    <label
+      className={`form-field-container ${
+        isFocused ? "form-field-container--focused" : ""
+      }`.trim()}
+    >
+      {label}
+      <input
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        className={className}
+        {...props}
+      />
+    </label>
+  );
 }
